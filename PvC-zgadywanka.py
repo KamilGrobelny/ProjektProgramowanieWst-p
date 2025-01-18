26d3552008e053381b78c3c835ab0b3a78b04e5a
bynio7 <piotrlubkowskill@gmail.com>
2025-01-18 15:31:45 +0100

    Aktualizacja pliku PvC-zgadywanka.py do wersji 1.0.2, dodanie liczby kroków i opisów argumentów wszystkich funkcji, drobne poprawki outputu

--- PvC-zgadywanka.py
+++ PvC-zgadywanka.py
@@ -1,11 +1,10 @@
 #Player vs computer, tylko czlowiek zgaduje
-"""
-v1.0.1
-"""
+"""v1.0.2"""
 from random import randint
-def PoprawnoscProby(proba: list, n: int, zgadywana: list):
-    """Funkcja sprawdza czy odgadnięcie podane przez użytkownika jest poprawnie wyrażone: odpowiednia długość n oraz same cyfry
-    jeżeli dane nie są poprawnie  wyrażone to funkcja zwraca True"""
+def PoprawnoscProby(proba: list, n: int):
+    """Funkcja sprawdza czy odgadnięcie podane przez użytkownika jest poprawnie wyrażone: odpowiednia długość n oraz same cyfry jeżeli dane nie są poprawnie  wyrażone to funkcja zwraca True
+    @ proba: odgadnięcie podane przez drugiego gracza, musi być listą pojedynczych cyfr o typie int
+    @ n: długość szyfru, która musi zawierać się w przedziale [1, 10]"""
     if len(proba) > n:
         print("Podano za długi szyfr, spróbuj jeszcze raz")
         return True
@@ -18,10 +17,13 @@ def PoprawnoscProby(proba: list, n: int, zgadywana: list):
             return True
 
 def InfoZwrotne(x: list, zgadywana: list, n: int):
-    """Funkcja zwraca tablicę z wartościami cyfr na właściwych oraz niewłaściwych miejscach
-    Tablica[0] to cyfry na właściwych miejscach
-    Tablica[1] to cyfry na niewłaściwych miejscach
-    Funkcja jest uniwersalna: można ją stosować do pvp, pvc, cvp"""
+    """Funkcja zwraca listę z wartościami cyfr na właściwych oraz niewłaściwych miejscach
+    Lista[0] to cyfry na właściwych miejscach
+    Lista[1] to cyfry na niewłaściwych miejscach
+    Funkcja jest uniwersalna: można ją stosować do pvp, pvc, cvp
+    @ x: odgadnięcie podane przez drugiego gracza, musi być listą pojedynczych cyfr o typie int
+    @ zgadywana: szyfr, który trzeba odgadnąć, podawany na początku gry przez pierwszego gracza
+    @ n: długość szyfru, która musi zawierać się w przedziale [1, 10]"""
     if x==zgadywana:
         wlasciwe = n
         niewlasciwe = 0
@@ -50,22 +52,27 @@ def InfoZwrotne(x: list, zgadywana: list, n: int):
         Tablica = [wlasciwe, niewlasciwe]
         return Tablica
               
-def Odgadywanie(zgadywana: list, n: int):
-    """Funkcja pobiera od użytkownika próbę. Za pomocą funkcji InfoZwrotne(x, zgadywana, n)
-    sprawdza czy próba == szyfr, a jeśli nie to zwraca informację ile cyfr jest na właściwych i niewłaściwych miejscach"""
-    proba = str(input("zgaduj: "))
+def Odgadywanie(zgadywana: list, n: int, krok: int):
+    """Funkcja pobiera od użytkownika próbę. Za pomocą funkcji InfoZwrotne(x: list, zgadywana: list, n:list)
+    sprawdza czy liczba cyfr na właściwych miejscach jest równa n, a jeśli nie to zwraca informację ile cyfr jest na właściwych i niewłaściwych miejscach
+    @ zgadywana: szyfr, który trzeba odgadnąć, podawany na początku gry przez pierwszego gracza
+    @ n: długość szyfru, która musi zawierać się w przedziale [1, 10]
+    @ krok: domyślnie (na początku gry, czyli przy pierwszym wywołaniu) jest równy 0, krok zwiększa się o 1 po wywołaniu funkcji InfoZwrotne"""
+    proba = str(input("Podaj swoje odgadnięcie: "))
     x = list(str(proba))
-    if PoprawnoscProby(x, n, zgadywana):
+    if PoprawnoscProby(x, n):
         return Odgadywanie(zgadywana, n)
     for i in range(len(x)):
         x[i]=int(x[i])
     Wartosci = InfoZwrotne(x, zgadywana, n)
+    krok+=1
     if Wartosci[0]==n:
+        print(f"wygrałeś, Twoja liczba odgadnięć: {krok}")
         return True
     else:
-        print(f"na właściwych miejscach: {Wartosci[0]}")
-        print(f"na niewłaściwych miejscach: {Wartosci[1]}")
-        return Odgadywanie(zgadywana, n)
+        print(f"Liczba cyfr występujących w szyfrze na właściwych miejscach: {Wartosci[0]}")
+        print(f"Liczba cyfr występujących w szyfrze, ale które nie są na właściwych miejscach: {Wartosci[1]}")
+        return Odgadywanie(zgadywana, n, krok)
 
 def PvC():
     """Funkcja umożliwia grę człowieka z komputerem, człowiek zgaduje szyfr komputera"""
@@ -79,9 +86,8 @@ def PvC():
         print("n musi być liczbą całowitą z przedziału [1, 10], podaj n ponownie")
         return PvC()
     zgadywana = []
-    zgadywana.append(randint(0, 9))
-    if Odgadywanie(zgadywana, n):
-        print("wygrałeś")
+    for i in range (n):
+        zgadywana.append(randint(0, 9))
+    if Odgadywanie(zgadywana, n, 0):
         return 0
 PvC()
-#End
