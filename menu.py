0debea09807974a0ce06300d1a6160b4e4b9ea24
Ignacy1 <iga.grz@op.pl>
2025-01-18 19:00:59 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,24 @@
 from random import randint
 import FunkcjeZgadywanka as FZ
 
-opcja=int(input("Wybierz opcje gry: \n 1 - zgadujesz szyfr wymyślony przez komputer \n 2 - komputer zgaduje szyfr wymyślony przez Ciebie \n 3 - grasz z drugim graczem \n"))
-if (opcja==1):
-    FZ.PvC()
-if (opcja==2):
-    print("czekamy na Janka")
-if (opcja==3):
-    opcja1 = int(input("Wybierz opcje gry: \n 1 - komputer losuje wam liczby \n 2 - sami wybieracie sobie liczby dla przecinika \n"))
-    if (opcja1 == 1):
-        FZ.PvP1()
-    if (opcja1 == 2):
-        FZ.PvP2()
+while(True):
+    opcja=int(input("Wybierz opcje gry: \n 1 - zgadujesz szyfr wymyślony przez komputer \n 2 - komputer zgaduje szyfr wymyślony przez Ciebie \n 3 - grasz z drugim graczem \n"))
+    if (opcja==1):
+        FZ.PvC()
+        break
+    if (opcja==2):
+        print("czekamy na Janka")
+        break
+    if (opcja==3):
+        while(True):
+            opcja1 = int(input("Wybierz opcje gry: \n 1 - komputer losuje wam liczby \n 2 - sami wybieracie sobie liczby dla przecinika \n"))
+            if (opcja1 == 1):
+                FZ.PvP1()
+                break
+            if (opcja1 == 2):
+                FZ.PvP2()
+                break
+            else:
+                print("Wybierz 1 lub 2.")
+    else:
+        print("Wybierz 1, 2 lub 3.")
