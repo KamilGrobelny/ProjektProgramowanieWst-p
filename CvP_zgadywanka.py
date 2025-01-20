55cb9a9bf714be31b0587470d32e691027804d9a
bynio7 <piotrlubkowskill@gmail.com>
2025-01-20 18:10:30 +0100

    Automatyczne testy do CvP_zgadywanka.py

--- CvP_zgadywanka.py
+++ CvP_zgadywanka.py
@@ -1,12 +1,12 @@
-haslo = ['x'] * 4  
+import FunkcjeZgadywanka as FZ
+from random import randint
 ilosc_ruchow = 0
-zapychacz = 0 #Liczba, o której wiemy, że jej nie ma
-
+zapychacz = 0 #Liczba, o której wiemy, że jej nie ma 
 n = int(input("Podaj długość hasła (n): "))
 if n > 10 or n < 1:
     raise ValueError("n musi być liczbą całkowitą z przedziału [1, 10]")
 
-def szukanie_liczb():
+def szukanie_liczb(zgadywana: list):
     global ilosc_ruchow, zapychacz
     mozliwe_liczby = []
     dostepne_liczby = list(range(10))  
@@ -15,18 +15,19 @@ def szukanie_liczb():
         if len(mozliwe_liczby)==n:
             return mozliwe_liczby
         proba = [i] * n
-        print("Próba:", "".join(map(str, proba)))
-        dobre_liczby = int(input("Ile liczb jest poprawnych? "))
-        liczby_na_swoim_miejscu = int(input("Ile liczb jest na swoich miejscach? "))
+        #print("Próba:", "".join(map(str, proba))) #na czas testów pomijamy to, żeby nie zaśmiecało outputu
+        tablica = FZ.InfoZwrotne(proba, zgadywana, n)
         ilosc_ruchow += 1
 
-        if dobre_liczby > 0:
-            mozliwe_liczby.extend([i] * dobre_liczby)
+        if tablica[1] > 0:
+            mozliwe_liczby.extend([i] * tablica[1])
+        if tablica[0] > 0:
+            mozliwe_liczby.extend([i] * tablica[0])
         else:
             zapychacz = i
     return mozliwe_liczby
 
-def szukanie_miejsca(mozliwe_liczby):
+def szukanie_miejsca(mozliwe_liczby, zgadywana: list):
     """Funkcja ustala miejsca dla poprawnych liczb w hasle."""
     global ilosc_ruchow
     miejsca = list(range(n)) 
@@ -35,27 +36,30 @@ def szukanie_miejsca(mozliwe_liczby):
         for miejsce in miejsca:
             proba = [zapychacz] * n
             proba[miejsce] = liczba
-            print("Próba:", "".join(map(str, proba)))
-            dobre_liczby = int(input("Ile liczb jest poprawnych? "))
-            liczby_na_swoim_miejscu = int(input("Ile liczb jest na swoich miejscach? "))
+            #print("Próba:", "".join(map(str, proba))) #to też pomijamy na czas testów
+            tablica = FZ.InfoZwrotne(proba, zgadywana, n)
             ilosc_ruchow += 1
 
-            if liczby_na_swoim_miejscu == 1:
+            if tablica[0] == 1:
                 haslo[miejsce] = liczba
                 miejsca.remove(miejsce)  
                 break
 
-def zgadywanie():
+def zgadywanie(zgadywana: list):
     """Główna funkcja obsługująca proces zgadywania."""
     global haslo
     haslo = ['x'] * n 
 
-    mozliwe_liczby = szukanie_liczb()
+    mozliwe_liczby = szukanie_liczb(zgadywana)
 
-    szukanie_miejsca(mozliwe_liczby)
+    szukanie_miejsca(mozliwe_liczby, zgadywana)
 
     finalna_proba = "".join(map(str, haslo))
-    print(f"Odgadnięte hasło: {finalna_proba}")
-    print(f"Liczba ruchów: {ilosc_ruchow}")
-
-zgadywanie()
+    print(f"SZYFR: {zgadywana}, Odgadnięte: {haslo}, ruchy {ilosc_ruchow}")
+for i in range(100):
+    zgadywana = []
+    #poniższą pętlę można wykasować i wprowadzać zgadywaną na sztywno, żeby sprawdzić konkretne przypadki i znaleźć słabość algorytmu
+    for j in range (n):
+        zgadywana.append(randint(0, 9))
+    zgadywanie(zgadywana)
+    ilosc_ruchow = 0
