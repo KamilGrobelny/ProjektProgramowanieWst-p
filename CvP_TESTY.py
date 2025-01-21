1be233575250d57196458227aad2869da68d481a
bynio7 <piotrlubkowskill@gmail.com>
2025-01-21 22:49:02 +0100

    od teraz n może być większe od 9

--- CvP_TESTY.py
+++ CvP_TESTY.py
@@ -1,16 +1,16 @@
 import FunkcjeZgadywanka as FZ
 from random import randint
 ilosc_ruchow = 0
-zapychacz = 9 #Liczba, o której wiemy, że jej nie ma 
 n = int(input("Podaj długość hasła (n): "))
-if n > 10 or n < 1:
-    raise ValueError("n musi być liczbą całkowitą z przedziału [1, 10]")
+#if n > 10 or n < 1:
+#    raise ValueError("n musi być liczbą całkowitą z przedziału [1, 10]")
 
 def szukanie_liczb(zgadywana: list):
-    global ilosc_ruchow, zapychacz
+    global ilosc_ruchow, zapychacz1, zapychacz2
+    zapychacz1 = [9, 0]
+    zapychacz2 = [8, 0]
     mozliwe_liczby = []
     dostepne_liczby = list(range(10))  
-
     for i in dostepne_liczby:
         if len(mozliwe_liczby)==n:
             return mozliwe_liczby
@@ -18,29 +18,43 @@ def szukanie_liczb(zgadywana: list):
         #print("Próba:", "".join(map(str, proba))) #na czas testów pomijamy to, żeby nie zaśmiecało outputu
         tablica = FZ.InfoZwrotne(proba, zgadywana, n)
         ilosc_ruchow += 1
-
+        if i==zapychacz1[0]:
+            JestZapychaczem1=True
+            JestZapychaczem2=False
+        elif i==zapychacz2[0]:
+            JestZapychaczem2=True
+            JestZapychaczem1=False
+        else:
+            JestZapychaczem1=False
+            JestZapychaczem2=False
+    
         if tablica[1] > 0:
             mozliwe_liczby.extend([i] * tablica[1])
+
         if tablica[0] > 0:
             mozliwe_liczby.extend([i] * tablica[0])
-        else:
-            zapychacz = i
+            if JestZapychaczem1:
+                zapychacz1[1]+=tablica[0]
+            elif JestZapychaczem2:
+                zapychacz2[1]+=tablica[0]
+
     return mozliwe_liczby
 
 def szukanie_miejsca(mozliwe_liczby, zgadywana: list):
     """Funkcja ustala miejsca dla poprawnych liczb w hasle."""
-    global ilosc_ruchow
+    global ilosc_ruchow, zapychacz1, zapychacz2
     miejsca = list(range(n)) 
-
+    AktualnyZapychacz=zapychacz1
     for liczba in mozliwe_liczby:
+        if liczba == zapychacz1[0]:
+            AktualnyZapychacz=zapychacz2
         for miejsce in miejsca:
-            proba = [zapychacz] * n
+            proba = [AktualnyZapychacz[0]] * n
             proba[miejsce] = liczba
             #print("Próba:", "".join(map(str, proba))) #to też pomijamy na czas testów
             tablica = FZ.InfoZwrotne(proba, zgadywana, n)
             ilosc_ruchow += 1
-
-            if tablica[0] == 1:
+            if tablica[0] == 1+AktualnyZapychacz[1]:
                 haslo[miejsce] = liczba
                 miejsca.remove(miejsce)  
                 break
@@ -56,12 +70,13 @@ def zgadywanie(zgadywana: list):
 
     finalna_proba = "".join(map(str, haslo))
     if zgadywana != haslo:
-        print(f"Something happening for {zgadywana}, returned {haslo} in {ilosc_ruchow} steps")
+        print(f"!!!SOMETHING HAPPENS for {zgadywana}, returned {haslo} in {ilosc_ruchow} steps")
+    else:
+        print(f"Nothing happens for {zgadywana}, returned {haslo} in {ilosc_ruchow} steps")
 
 ilosc_prob=int(input("podaj liczbę losowych przykładów, które chcesz sprawdzić: "))
 for i in range(ilosc_prob):
     zgadywana = []
-    zapychacz = 9
     #poniższą pętlę można wykasować i wprowadzać zgadywaną na sztywno, żeby sprawdzić konkretne przypadki i znaleźć słabość algorytmu
     for j in range (n):
         zgadywana.append(randint(0, 9))
